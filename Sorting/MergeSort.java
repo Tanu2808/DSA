8428a2b80e23b9ba280f61377216fd7ea16471ab
Tanush <tanushv28@gmail.com>
2026-06-24 15:00:28 +0530

    Sorting: Merge Sort

---
+++ Sorting/MergeSort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+public class MergeSort {
+    public static void sort(int nums[])
+    {
+        int low = 0;
+        int high = nums.length - 1;
+        mergeSort(nums, low, high); 
+    }
+
+    private static void mergeSort(int[] nums, int low, int high) {
+        if (low >= high) return;
+        int mid = low + (high - low) / 2;
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid + 1, high);
+        merge(nums, low, mid, high);
+    }
+
+    private static void merge(int[] nums, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+
+        int left = low;
+        int right = mid + 1;
+        int k = 0;
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) temp[k++] = nums[left++];
+            else temp[k++] = nums[right++];
+        }
+        while (left <= mid) temp[k++] = nums[left++];
+        while (right <= high) temp[k++] = nums[right++];
+
+        for (int i = 0; i < temp.length; i++) nums[low + i] = temp[i];
+    }
+}
