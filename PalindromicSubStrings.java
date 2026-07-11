077154eaecac2ee82fc35e2ba087f9931a809be2
Tanush <tanushv28@gmail.com>
2026-07-11 19:17:18 +0530

    Leetcode 647: Palindromic Substrings

---
+++ PalindromicSubStrings.java
@@ -0,0 +1,21 @@
+public class PalindromicSubStrings {
+    public int expand(String s, int left, int right)
+    {
+        int answer = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            right++;
+            left--;
+            answer++;
+        }
+        return answer;
+        
+    }
+    public int countSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count += expand(s, i, i);
+            count += expand(s, i, i + 1);
+        }
+        return count;
+    }   
+}
