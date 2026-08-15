434b40aecf53bb6b050c059e2f3f1c28cd7dd588
Tanush <tanushv28@gmail.com>
2026-08-15 12:21:47 +0530

    Leetcode 1413: Minimum Value to Get Positive Step by Step Sum

---
+++ MinimumValuePositiveSum.java
@@ -0,0 +1,11 @@
+public class MinimumValuePositiveSum {
+    public int minStartValue(int[] nums) {
+        int startValue = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            startValue = Math.min(startValue, sum);
+        }
+        return 1 - startValue;
+    }
+}
